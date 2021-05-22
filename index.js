16eb7c28e05e6e113b198039717623ecb3ed883c
Michael Martinez <zeroeclipse@live.com>
2021-05-22 11:36:07 -0400

    commit tree 486dbe4f7873b59606b4a8ce5168e3b36e6cd7f8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    let matching = drivers.filter( n => {
+        return n.toLowerCase() == string.toLowerCase();
+    })
+    return matching
+}
+
+
+function fuzzyMatch(drivers, string) {
+    let matching = drivers.filter( n => {
+        return n.indexOf(string) === 0
+    })
+    return matching
+}
+
+function matchName(drivers, string){
+    let matching = drivers.filter( n => {
+        return n.name == string;
+    })
+    return matching
+}
